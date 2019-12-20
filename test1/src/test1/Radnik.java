8010b57cdedd3e9ed0f2038a7805d96138f3afbe
HP Omen <HP Omen@DESKTOP-1QEJTP6>
2019-12-20 13:55:06 +0100

    pocetno stanje

---
+++ test1/src/test1/Radnik.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author HP Omen
+ */
+public class Radnik {
+    private String ime;
+    private String prezime;
+    private int brojTelefona;
+    
+
+    public Radnik(String ime, String prezime, int brojTelefona) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.brojTelefona = brojTelefona;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public int getBrojTelefona() {
+        return brojTelefona;
+    }
+
+    public void setBrojTelefona(int brojTelefona) {
+        this.brojTelefona = brojTelefona;
+    }
+    
+    
+}
