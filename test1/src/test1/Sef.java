a409264bff491c722832d0a12c2270095843a1d2
balasevics <balasevics@hotmail.rs>
2019-12-20 14:03:08 +0100

    Ovo je moja izmena

---
+++ test1/src/test1/Sef.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author INTEL I7-8700
+ */
+public class Sef {
+    
+    private String Ime;
+
+    public Sef(String Ime) {
+        this.Ime = Ime;
+    }
+
+    public String getIme() {
+        return Ime;
+    }
+
+    public void setIme(String Ime) {
+        this.Ime = Ime;
+    }
+    
+    
+    
+    
+}
